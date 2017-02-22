9c829510371969459cdf626283a25a92ee8af11d
katalinkovacs <katalin.kovacs@marlo.com.au>
2017-02-22 15:39:48 +1100

    Working version.
    Changed amq to file. --> Working too.

--- TransformationXmlToXml/src/main/java/routes/RoutePerson1ToPerson2.java
+++ TransformationXmlToXml/src/main/java/routes/RoutePerson1ToPerson2.java
@@ -17,7 +17,9 @@ public class RoutePerson1ToPerson2 extends RouteBuilder{
 
             JaxbDataFormat jaxbDataFormatPerson2 = new JaxbDataFormat("personinfo2");
 
-            from("amq:data.in")
+          // FROM AMQ TO AMQ
+
+          /*  from("amq:data.in")
                     //unmarshalling is the process to read in xml and convert to java object using jaxb generated template
                     // classes
                     .unmarshal(jaxbDataFormatPerson1)
@@ -27,6 +29,24 @@ public class RoutePerson1ToPerson2 extends RouteBuilder{
                     // a person2 which will be used to create the output xml
                     .marshal(jaxbDataFormatPerson2)
                     .to("amq:data.out");
+         */
+
+
+          // FROM FILE TO FILE
+            
+            from("file:C:/Kati/MarloPractise/FileReadInOut/From")
+                    //unmarshalling is the process to read in xml and convert to java object using jaxb generated template
+                    // classes
+                    .unmarshal(jaxbDataFormatPerson1)
+                    //this is to invoke processor to transform person1 to person2
+                    .bean(processor, "myprocessor")
+                    //marshalling is the process to convert java to xml -- the processor from previous step returns
+                    // a person2 which will be used to create the output xml
+                    .marshal(jaxbDataFormatPerson2)
+                    .to("file:C:/Kati/MarloPractise/FileReadInOut/To");
+
+
+
 
         }
 
